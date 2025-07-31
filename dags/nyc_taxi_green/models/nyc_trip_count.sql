74201bb66bf9572dba2808de3bcab27fb0c154c2
pedrojunqueira <pedro.junqueira@agile-analytics.com.au>
2025-07-31 16:08:01 +0930

    initial commit

---
+++ dags/nyc_taxi_green/models/nyc_trip_count.sql
@@ -0,0 +1,17 @@
+with new_york_taxis as (
+       select * from nyctlc
+   ),
+   final as (
+     SELECT
+       vendorID,
+       CAST(lpepPickupDatetime AS DATE) AS trip_date,
+       COUNT(*) AS trip_count
+     FROM
+         [nyc_taxi_sample].[dbo].[nyctlc]
+     GROUP BY
+         vendorID,
+         CAST(lpepPickupDatetime AS DATE)
+   
+   )
+   select * from final
+   
